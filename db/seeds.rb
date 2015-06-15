5ebe3d7ff411cbf6604a1e77b3aa3616da65e582
David P. Hanesana <dhanesana@gmail.com>
2015-06-14 21:50:42 -0700

    Update seed for negative members bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@ while page_num < 501 # First 500 Pages
     name = page.css('td.cafename a substring')[item_num].text
     link = page.css('td.cafename')[item_num].css('a').first['href']
     members = page.css('td.member')[item_num].text.gsub(/,/, '').to_f
+    members = 0 if members < 1
     Cafe.create(title: name, score: members, url: link)
     item_num += 1
   end
