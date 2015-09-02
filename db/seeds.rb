be9f66f8ab21514b77cf523d49c87916c27d1afb
David P. Hanesana <dhanesana@gmail.com>
2015-09-02 03:19:38 -0700

    Scrape first 5000 pages, and only save entries with 1000+ members

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ require 'open-uri'
 
 page_num = 1
 
-while page_num < 251 # First 500 Pages
+while page_num < 5000 # First 5000 Pages
   unless Cafe.last.nil?
     break if Cafe.last.id > 9449
   end
@@ -12,14 +12,14 @@ while page_num < 251 # First 500 Pages
     name = page.css('td.cafename a substring')[item_num].text
     link = page.css('td.cafename')[item_num].css('a').first['href']
     members = page.css('td.member')[item_num].text.gsub(/,/, '').to_f
-    members = 0 if members < 1
+    # members = 0 if members < 1
     @cafe = Cafe.where(title: name).first
     if @cafe
       @cafe.url = link
       @cafe.score = members
       @cafe.save
     else
-      Cafe.create(title: name, score: members, url: link)
+      Cafe.create(title: name, score: members, url: link) if members > 999
     end
     item_num += 1
   end
